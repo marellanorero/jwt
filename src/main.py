32608ea4d9344a4dead947015b016eaae983786a
marellanorero <marellanonorero@gmail.com>
2022-08-21 21:18:03 +0000

    jwt: register, login and private pages

---
+++ src/main.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+import datetime
+
+
+app = Flask(__name__)
+app.url_map.slashes = False
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["JWT_SECRET_KEY"] =  'dc7d98009d61ac52e4d1b64de55b7165'
+
+db.init_app(app)
+Migrate(app, db)
+jwt = JWTManager(app)
+CORS(app)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/api/register', methods=['POST'])
+def register():
+
+
+    #traigo los datos
+    name=request.json.get('name', "")
+    email=request.json.get('email')
+    password=request.json.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user : return jsonify({ "msg": " Existe una cuenta con este email. "}), 400
+
+    #creo el usuario
+    user = User()
+    user.name = name
+    user.email = email
+    user.password = generate_password_hash(password)
+    user.save()
+
+    return jsonify({ "msg": "Usuario registrado, por favor, inicie sesión" }), 201
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    
+    email=request.json.get('email')
+    password=request.json.get('password')
+
+    user = User.query.filter_by(email=email, isActive=True).first()
+    if not user : return jsonify({ "msg": "Usuario y/o contraseña incorrectos. "}), 400
+
+    if not check_password_hash(user.password, password): return jsonify({ "msg" : "Usuario y/o contraseña no se encuentra/n"})
+
+    expire= datetime.timedelta(days=3)
+
+    print(expire)
+
+    access_token =  create_access_token(identity=user.email, expires_delta=expire)
+
+    data = {
+        "access_token" : access_token,
+        "user" : user.serialize()
+    }
+
+    return jsonify(data), 200
+
+@app.route('/api/users', methods=['GET'])
+@jwt_required()
+def users():
+
+    users = User.query.all()
+    users = list(map(lambda user : user.serialize(), users))
+
+    return jsonify(users), 200
+
+@app.route('/api/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    identity = get_jwt_identity()
+    user = User.query.filter_by(email=identity).first()
+    return jsonify({ "identity": identity, "user": user.serialize() }), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ ='users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), default="")
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    isActive = db.Column(db.Boolean(), default=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "isActive": self.isActive
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
